e5a50a89f5f4d08d346f97d0b83bb1947c6d81d7
Greg Wanner <greg@tegogroup.com>
2024-03-06 12:37:49 -0500

    Completion of Exercise 2

---
+++ FactoryPatternExercise2/ListDataAccess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    public class ListDataAccess : IDataAccess
+    {
+        public List<Product> LoadData()
+        {
+            Console.WriteLine("I am reading data from List database.");
+            return new List<Product>();
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("I am saving data to a List database.");
+        }
+    }
+}
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            string uDBType;
+            Console.WriteLine("Please enter the database you wish to use (List, SQL, or Mongo:");
+            uDBType = Console.ReadLine();
+            IDataAccess db1 = DataAccessFactory.GetDataAccessType(uDBType);
+            var nProducts = db1.LoadData();
+            db1.SaveData();
         }
     }
 }
